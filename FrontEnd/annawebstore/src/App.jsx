2fc393351e1617535d40787651a7ee57c0ced344
developeralain <93736888+developeralain@users.noreply.github.com>
2023-03-01 00:10:04 -0500

    reset

--- FrontEnd/annawebstore/src/App.jsx
+++ FrontEnd/annawebstore/src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-// MATERIAL US IMPORTS
-import Button from '@mui/material/Button'
-
-// END MATERIAL US IMPORTS
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Vite + React</h1>
-      <Button variant="contained">Hello World</Button>
       <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
